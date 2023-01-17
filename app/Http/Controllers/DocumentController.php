0881998a046f2ed5eaa900227a6a6a13c907d479
flaviodematoshenrique <digitalclasse@gmail.com>
2023-01-17 11:51:01 -0300

    first commit

---
+++ app/Http/Controllers/DocumentController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DocumentController extends Controller
+{
+
+    public function store(Request $request) {
+        $name_document = $request->file->name;
+
+        $request->file->storeAs('/public/documents', $name_document);
+    }
+
+}
